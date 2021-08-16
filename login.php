d272d64f1d48f2232c1558c82f230c46190f4b0f
wojtix12345 <88909086+wojtix12345@users.noreply.github.com>
2021-08-16 15:43:10 +0200

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$login = $_POST['login'];
-$hasło = $_POST['hasło'];
-$tekst = "login: $login hasło: $hasło \r\n";
-
-$uchwyt = fopen("login.txt", "a");
-fwrite($uchwyt, $tekst);
-fclose($uchwyt);
